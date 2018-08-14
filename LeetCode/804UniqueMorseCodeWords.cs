de624f9fe209ba65ed7a18ef5ada232f5f5b4a84
Xavier Xie <xieyuehao@gmail.com>
2018-08-14 19:27:02 +1000

    UniqueMorseRepresentations

---
+++ LeetCode/804UniqueMorseCodeWords.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _804UniqueMorseCodeWords
+    {
+        //International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+        //For convenience, the full table for the 26 letters of the English alphabet is given below:
+        //            [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+        //Example:
+        //Input: words = ["gin", "zen", "gig", "msg"]
+        //Output: 2
+        //Explanation: 
+        //The transformation of each word is:
+        //"gin" -> "--...-."
+        //"zen" -> "--...-."
+        //"gig" -> "--...--."
+        //"msg" -> "--...--."
+
+        //There are 2 different transformations, "--...-." and "--...--.".
+
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            var result = new Dictionary<string, int>();
+            var a = 97;
+       
+            var morseCode = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            var hash = new Dictionary<char, string>();
+            for (int i = 0; i < morseCode.Length; i++)
+            {
+                hash.Add(Convert.ToChar(i + a), morseCode[i]);
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var wordMorse = "";
+
+                for (int k = 0; k < words[i].Length; k++)
+                {
+                    var c = words[i][k];
+                    wordMorse = wordMorse + hash[c];
+                }
+                if (result.ContainsKey(wordMorse))
+                {
+                    result[wordMorse]++;
+                }
+                else
+                {
+                    result.Add(wordMorse, 1);
+
+                }
+            }
+
+            return result.Count;
+        }
+
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -12,6 +12,23 @@ namespace LeetCode
         {
             var sol = new Solution();
 
+            var i = 0;
+            var d = 0.2;
+            var s = "@#$";
+            var newInt = Convert.ToInt32(Console.ReadLine());
+            var newDouble = Convert.ToDouble(Console.ReadLine());
+            var newStr = Console.ReadLine();
+
+            // Read and save an integer, double, and String to your variables.
+
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(i + newInt);
+
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine(d + newDouble);
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine(s + newStr);
         }
 
         public class Solution

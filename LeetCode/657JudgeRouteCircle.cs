4b9527e781d1cd83e1af3fe327405b7a485db66a
Xavier Xie <xieyuehao@gmail.com>
2018-08-15 22:37:59 +1000

    JudgeCircle

---
+++ LeetCode/657JudgeRouteCircle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _657JudgeRouteCircle
+    {
+        public bool JudgeCircle(string moves)
+        {
+            var result = false;
+            var l = 0;
+            var r = 0;
+            var u = 0;
+            var d = 0;
+            for (int i = 0; i < moves.Length; i++)
+            {
+                if (moves[i] == 'L' || moves[i] == 'l')
+                {
+                    l++;
+                }
+                if (moves[i] == 'r' || moves[i] == 'R')
+                {
+                    r++;
+                }
+                if (moves[i] == 'u' || moves[i] == 'U')
+                {
+                    u++;
+                }
+                if (moves[i] == 'd' || moves[i] == 'D')
+                {
+                    d++;
+                }
+            }
+            if (l == r && u == d) { result = true; }
+            else { result = false; }
+
+            return result;
+        }
+    }
+
+}
---
+++ LeetCodeTests/_657JudgeRouteCircleTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _657JudgeRouteCircleTests
+    {
+        [TestMethod()]
+        public void JudgeCircleTest()
+        {
+            var j = new _657JudgeRouteCircle();
+
+            var result = j.JudgeCircle("UD");
+
+            Assert.IsTrue(result);
+        }
+    }
+}

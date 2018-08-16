8e23c41bcb7fbf2e6aceca9723de0cdf20a5c31e
Xavier Xie <xieyuehao@gmail.com>
2018-08-16 20:08:00 +1000

    _344ReverseStringTests

---
+++ LeetCode/344ReverseString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+   public class _344ReverseString
+    {
+        public string ReverseString(string s)
+        {
+            var result = new char[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                result[s.Length - i - 1] = s[i];
+            }
+            return new String(result);
+        }
+    }
+}
---
+++ LeetCodeTests/_344ReverseStringTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _344ReverseStringTests
+    {
+        [TestMethod()]
+        public void ReverseStringTest()
+        {
+            var re = new _344ReverseString();
+            var result = re.ReverseString("ABC ADD : uisd");
+            Assert.AreEqual(result, "dsiu : DDA CBA");
+        }
+    }
+}

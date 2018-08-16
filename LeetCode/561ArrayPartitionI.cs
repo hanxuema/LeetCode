8b612179bd83649d29c3dff8245d3e0c3ea19775
Xavier Xie <xieyuehao@gmail.com>
2018-08-16 20:37:20 +1000

    _561ArrayPartitionI

---
+++ LeetCode/561ArrayPartitionI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+   public class _561ArrayPartitionI
+    {
+        public int ArrayPairSum(int[] nums)
+        {
+            var result = 0;
+            //sort first
+             Array.Sort(nums);
+            //and take every 2 pair
+            for (int i = 0; i < nums.Count(); i+=2)
+            {
+                result = result + nums[i];
+            }
+            return result;
+        }
+    }
+}

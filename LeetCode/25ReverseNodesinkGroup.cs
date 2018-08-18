3ce1cbc65eb99a3685b7c27de680c350e193074c
Xavier Xie <hanxuema@hotmail.com>
2018-08-18 10:03:39 +1000

    temp reverse linkedlist

---
+++ LeetCode/25ReverseNodesinkGroup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class _25ReverseNodesinkGroup
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        { 
+            ListNode next = null;
+            var currentIndex = 1;
+            while (head != null)
+            {
+                next = head.next;
+                next.next = head;
+                head = next;
+                currentIndex++;
+                if (currentIndex == k)
+                {
+                    head.next = null;
+                    break;
+                }
+            }
+            return head;
+        }
+    }
+}
---
+++ LeetCodeTests/_25ReverseNodesinkGroupTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _25ReverseNodesinkGroupTests
+    {
+        [TestMethod()]
+        public void ReverseKGroupTest()
+        {
+            //1->2->3->4->5
+            var ar = new int[] { 1, 2, 3, 4, 5 };
+            ListNode node = null;
+            ListNode currentNode = null;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                var newNode = new ListNode(ar[i]);
+                if (node == null)
+                {
+                    node = newNode;
+                }
+                currentNode.next = newNode;
+            }
+
+
+            Assert.Fail();
+        }
+    }
+}

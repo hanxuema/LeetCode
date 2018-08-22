4a12d067c8375b3e8749644666875c076784375d
Xavier Xie <xieyuehao@gmail.com>
2018-08-22 23:41:54 +1000

    _728SelfDividingNumbers

---
+++ LeetCodeTests/_728SelfDividingNumbersTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _728SelfDividingNumbersTests
+    {
+        [TestMethod()]
+        public void SelfDividingNumbersTest()
+        {
+            var sdn = new _728SelfDividingNumbers();
+            var rst = sdn.SelfDividingNumbers(1, 22);
+
+            Assert.IsTrue(rst.ToArray().SequenceEqual(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22 }));
+        }
+    }
+}

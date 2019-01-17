63d3efab7b5a73760611345a30de0c1d11b69325
Xavier Xie <xieyuehao@gmail.com>
2019-01-17 22:23:22 +1000

    redo two sum

--- LeetCode/1TwoSum.cs
+++ LeetCode/1TwoSum.cs
@@ -8,25 +8,44 @@ namespace LeetCode
 {
     public class _1TwoSum
     {
+        //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+        //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        //Example:
+
+        //Given nums = [2, 7, 11, 15], target = 9,
+
+        //Because nums[0] + nums[1] = 2 + 7 = 9,
+        //return [0, 1].
         public int[] TwoSum(int[] nums, int target)
         {
+            var result = new int[] { -1, -1 };
 
-            int[] result = new int[2];
-            Dictionary<int, int> dict = new Dictionary<int, int>();
+            //check corner case
+            if (nums == null || nums.Length < 2)
+            {
+                return result;
+            }
+            var dic = new Dictionary<int, int>();
             for (int i = 0; i < nums.Length; i++)
             {
-                if (dict.ContainsKey(target - nums[i]))
+                //if the dic has no such value, add it
+                if (dic.ContainsKey(target - nums[i])) //dic has 2 and target - nums[i] is 7
                 {
+                    //find the value
+                    result[0] = dic[target - nums[i]];
                     result[1] = i;
-                    result[0] = dict[target - nums[i]];
-                    return result;
+                    break;
+                    
                 }
-                if (!dict.ContainsKey(nums[i]))
+                //else return
+                if (!dic.ContainsKey(nums[i]))
                 {
-                    dict.Add(nums[i], i);
+                    dic.Add(nums[i], i);
                 }
             }
             return result;
         }
     }
-} 
+}
---
+++ LeetCode/844BackspaceStringCompare.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _844BackspaceStringCompare
+    {
+        //Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+        //Example 1:
+
+        //Input: S = "ab#c", T = "ad#c"
+        //Output: true
+        //Explanation: Both S and T become "ac".
+        //Example 2:
+
+        //Input: S = "ab##", T = "c#d#"
+        //Output: true
+        //Explanation: Both S and T become "".
+        //Example 3:
+
+        //Input: S = "a##c", T = "#a#c"
+        //Output: true
+        //Explanation: Both S and T become "c".
+        //Example 4:
+
+        //Input: S = "a#c", T = "b"
+        //Output: false
+        //Explanation: S becomes "c" while T becomes "b".
+        //Note:
+
+        //1 <= S.length <= 200
+        //1 <= T.length <= 200
+        //S and T only contain lowercase letters and '#' characters.
+        //Follow up:
+
+        //Can you solve it in O(N) time and O(1) space?
+        public bool BackspaceCompare(string S, string T)
+        {
+            var result = true;
+            if (string.IsNullOrEmpty(S) && string.IsNullOrEmpty(T))
+            {
+                return result;
+            }
+            //set 2 pointers
+            var p1 = S.Length - 1;
+            var p2 = T.Length - 1;
+
+            return result;
+        }
+    }
+}

56934c242f95fe3a0466ec477d543835db9381a2
Xavier Xie <xieyuehao@gmail.com>
2019-01-20 22:49:44 +1000

    134 gas station, need to understand it deeper

---
+++ LeetCodeTests/_134GasStationTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _134GasStationTests
+    {
+        [TestMethod()]
+        public void CanCompleteCircuitShouldReturnMunis1()
+        {
+            var ccc = new _134GasStation();
+            var gas = new int[] { 2,3,4};
+            var cost = new int[] { 3, 4, 3 };
+            var value = ccc.CanCompleteCircuit(gas, cost);
+
+            Assert.AreEqual(-1, value);
+        }
+
+        [TestMethod()]
+        public void CanCompleteShouldReturn3()
+        {
+            var ccc = new _134GasStation();
+            var gas = new int[] { 1, 2, 3, 4, 5 };
+            var cost = new int[] { 3, 4, 5, 1, 2 };
+            var value = ccc.CanCompleteCircuit(gas, cost);
+
+            Assert.AreEqual(3, value);
+        }
+    }
+}

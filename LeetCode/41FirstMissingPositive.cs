9fd8ccb862a2041b51af2d54d54931f974c05810
Xavier Xie <xieyuehao@gmail.com>
2019-01-20 16:32:19 +1000

    41 firstmissing positive version 1

---
+++ LeetCode/41FirstMissingPositive.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given an unsorted integer array, find the smallest missing positive integer.
+
+    //Example 1:
+    //Input: [1,2,0]
+    //Output: 3
+
+    //Example 2:
+    //Input: [3,4,-1,1]
+    //Output: 2
+
+    //Example 3:
+    //Input: [7,8,9,11,12]
+    //Output: 1
+
+    //Note:
+
+    //Your algorithm should run in O(n) time and uses constant extra space.
+    public class _41FirstMissingPositive
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 1;
+            }
+
+            //make all the items that less than 0 to be the max
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] <= 0)
+                {
+                    nums[i] = int.MaxValue;
+                }
+            }
+
+            //
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = Math.Abs(nums[i]);
+                if (num <= nums.Length)
+                {
+                    nums[num - 1] = -Math.Abs(nums[num - 1]);
+                }
+            }
+
+            
+            //return the index of first non-zero value
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    return i + 1;
+                }
+            }
+
+            return nums.Length + 1;
+        }
+    } 
+}
---
+++ LeetCodeTests/_41FirstMissingPositiveTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _41FirstMissingPositiveTests
+    {
+        [TestMethod()]
+        public void FirstMissingPositiveTest()
+        {
+            var fmp = new _41FirstMissingPositive();
+
+            var result = fmp.FirstMissingPositive(new int[] { 2, 1 });
+            Assert.AreEqual(result, 3);
+
+            result = fmp.FirstMissingPositive(new int[] { 3, 4, -1, 1 });
+            Assert.AreEqual(result, 2);
+
+            result = fmp.FirstMissingPositive(new int[] { 7, 8, 9, 11, 12 });
+            Assert.AreEqual(result, 1);
+
+
+        }
+    }
+}

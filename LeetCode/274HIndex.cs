9a6049323972377e1d9079777cc52003708cf93a
Xavier Xie <xieyuehao@gmail.com>
2019-01-21 21:47:47 +1000

    274HIndex

---
+++ LeetCode/274HIndex.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given an array of citations(each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+    //According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+    //Example:
+
+    //Input: citations = [3,0,6,1,5]
+    //    Output: 3 
+    //Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had
+    //             received 3, 0, 6, 1, 5 citations respectively.
+    //             Since the researcher has 3 papers with at least 3 citations each and the remaining
+    //             two with no more than 3 citations each, her h-index is 3.
+    //Note: If there are several possible values for h, the maximum one is taken as the h-index.
+    public class _274HIndex
+    {
+        public int HIndex(int[] citations)
+        {
+            //check corner case
+            if (citations == null || citations.Length == 0) return 0;
+            var result = 0;
+            //sort the citations
+            Array.Sort(citations);
+            //for loop and check the "distance" to the end of the array 
+            for (int i = citations.Length - 1; i >= 0; i--)
+            {
+                int h = citations.Length - i;
+                if (citations[i] < h)
+                {
+                    return h - 1;
+                }
+            }
+            return citations.Length;
+        }
+    }
+}
---
+++ LeetCodeTests/_274HIndexTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _274HIndexTests
+    {
+        [TestMethod()]
+        public void HIndexShouldReturn3()
+        {
+            var hi = new _274HIndex();
+            var result = hi.HIndex(new int[] { 3, 0, 6, 1, 5 });
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod()]
+        public void HIndexShouldReturn4()
+        {
+            var hi = new _274HIndex();
+            var result = hi.HIndex(new int[] { 7,4,6,2,4 });
+            Assert.AreEqual(4, result);
+        }
+    }
+}

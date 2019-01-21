7b37dc4d56d8d1e7da4d153e7c53a0d7107b387c
Xavier Xie <xieyuehao@gmail.com>
2019-01-21 23:15:55 +1000

    217ContainsDuplicate

---
+++ LeetCode/217ContainsDuplicate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given an array of integers, find if the array contains any duplicates.
+
+    //Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+    //Example 1:
+    //Input: [1,2,3,1]
+    //    Output: true
+
+    //Example 2:
+    //Input: [1,2,3,4]
+    //    Output: false
+
+    //Example 3:
+    //Input: [1,1,1,3,3,4,3,2,4,2]
+    //    Output: true
+
+    public class _217ContainsDuplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+
+            //check corner case
+            if (nums == null || nums.Length <= 1) return false;
+            //loop
+            var result = false;
+            var hs = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!hs.Contains(nums[i]))
+                {
+                    hs.Add(nums[i]);
+                }
+                else
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/_217ContainsDuplicateTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _217ContainsDuplicateTests
+    {
+        [TestMethod()]
+        public void ContainsDuplicateTest()
+        {
+            var cd = new _217ContainsDuplicate();
+            var result = cd.ContainsDuplicate(new int[] {3,3});
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}

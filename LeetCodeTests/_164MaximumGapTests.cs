7448e1fa0a16334cd9d8eab4053b941949199856
Xavier Xie <xieyuehao@gmail.com>
2019-01-27 20:52:18 +1000

    164Max gap first version sort nlogn

---
+++ LeetCodeTests/_164MaximumGapTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _164MaximumGapTests
+    {
+        [TestMethod()]
+        public void MaximumGapTest()
+        {
+            var mg = new _164MaximumGap();
+            var result = mg.MaximumGap(new int[] { 3, 6, 9, 1 });
+            Assert.AreEqual(3,result);
+        }
+
+        [TestMethod()]
+        public void MaximumGapShouldReturn0()
+        {
+            var mg = new _164MaximumGap();
+            var result = mg.MaximumGap(new int[] { 10 });
+            Assert.AreEqual(0, result);
+        }
+    }
+}

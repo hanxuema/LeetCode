097e0cabe39e97dcfa39a38bfc03c90987126a4b
Xavier Xie <xieyuehao@gmail.com>
2019-01-27 15:47:19 +1000

    334 increasing triplet

---
+++ LeetCode/334IncreasingTripletSubsequence.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+
+    //Formally the function should:
+
+    //Return true if there exists i, j, k
+    //such that arr[i] < arr[j] < arr[k] given 0 ≤ i<j<k ≤ n-1 else return false.
+    //Note: Your algorithm should run in O(n) time complexity and O(1) space complexity.
+
+    //Example 1:
+
+    //Input: [1,2,3,4,5]
+    //Output: true
+    //Example 2:
+
+    //Input: [5,4,3,2,1]
+    //Output: false
+    public class _334IncreasingTripletSubsequence
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            //check corner case
+            if (nums == null || nums.Length < 3) return false;
+
+            var result = false;
+            var min1 = int.MaxValue;
+            var min2 = int.MaxValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var cur = nums[i];
+                if (cur < min1)
+                {
+                    min1 = cur;
+                }
+                else if (cur > min1 && cur < min2)
+                {
+                    min2 = cur;
+                }
+                else if (cur > min2)
+                {
+                    return true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/_334IncreasingTripletSubsequenceTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _334IncreasingTripletSubsequenceTests
+    {
+        [TestMethod()]
+        public void IncreasingTripletTest()
+        {
+            var it = new _334IncreasingTripletSubsequence();
+            var result = it.IncreasingTriplet(new int[] { 1, 2, 3, 4, 5 });
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod()]
+        public void IncreasingTripletShouldReturnFalse()
+        {
+            var it = new _334IncreasingTripletSubsequence();
+            var result = it.IncreasingTriplet(new int[] { 5, 4, 3, 2, 1 });
+            Assert.AreEqual(false, result);
+        }
+
+
+        [TestMethod()]
+        public void IncreasingTripletShouldReturnTrue()
+        {
+            var it = new _334IncreasingTripletSubsequence();
+            var result = it.IncreasingTriplet(new int[] { 5, 1, 5, 5, 2, 5, 4 });
+            Assert.AreEqual(true, result);
+        }
+    }
+}

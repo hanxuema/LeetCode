f302bf7113fb43da700c448777e0fdb8d621adf3
Xavier Xie <xieyuehao@gmail.com>
2019-01-27 21:13:42 +1000

    287Find the duplicate number

---
+++ LeetCodeTests/_287FindtheDuplicateNumberTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _287FindtheDuplicateNumberTests
+    {
+        [TestMethod()]
+        public void FindDuplicateTest()
+        {
+            var fd = new _287FindtheDuplicateNumber();
+            var result = fd.FindDuplicate(new int[] { 1, 3, 4, 2, 2 });
+            Assert.AreEqual(2,result);
+        }
+
+        [TestMethod()]
+        public void FindDuplicateShouldReturn3()
+        {
+            var fd = new _287FindtheDuplicateNumber();
+            var result = fd.FindDuplicate(new int[] { 3, 1, 3, 4, 2 });
+            Assert.AreEqual(3, result);
+        }
+
+
+ 
+    }
+}

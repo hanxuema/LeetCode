230226d26b96b426bf1cc0f06ae9568e9e79a951
Xavier Xie <xieyuehao@gmail.com>
2019-01-28 14:52:55 +1000

    14 longest common prefix

---
+++ LeetCode/14LongestCommonPrefix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Write a function to find the longest common prefix string amongst an array of strings.
+
+    //If there is no common prefix, return an empty string "".
+
+    //Example 1:
+
+    //Input: ["flower","flow","flight"]
+    //Output: "fl"
+    //Example 2:
+
+    //Input: ["dog","racecar","car"]
+    //Output: ""
+    //Explanation: There is no common prefix among the input strings.
+    //Note:
+
+    //All given inputs are in lowercase letters a-z.
+    public class _14LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length < 2) return "";
+            if (strs.Length == 1) return strs[0];
+
+            var result = new StringBuilder(strs[0]);
+            for (int i = 1; i < strs.Length; i++)
+            { 
+                //make sure it compare with the result not the pre one
+                var temp = result.ToString();
+                var cur = strs[i];
+                var shortLength = Math.Min(cur.Length, temp.Length);
+                if (shortLength == 0) return "";
+
+                result.Clear();
+                for (int k = 0; k < shortLength; k++)
+                {
+                    if (cur[k] == temp.ToString()[k])
+                    {
+                        result.Append(cur[k]);
+                    }
+                    else break;
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ LeetCodeTests/_14LongestCommonPrefixTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _14LongestCommonPrefixTests
+    {
+        [TestMethod()]
+        public void LongestCommonPrefixTest()
+        {
+            var lcp = new _14LongestCommonPrefix();
+            var result = lcp.LongestCommonPrefix(new string[] { "flower", "flow", "flight" });
+            Assert.AreEqual("fl", result);
+        }
+
+        [TestMethod()]
+        public void LongestCommonPrefixShouldReturnEmptyString()
+        {
+            var lcp = new _14LongestCommonPrefix();
+            var result = lcp.LongestCommonPrefix(new string[] { "b", "cb", "cab" });
+            Assert.AreEqual("", result);
+        }
+    }
+}

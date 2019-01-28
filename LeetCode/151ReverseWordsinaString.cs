f4ab1df0e9e018395953a8a7e182376b0b85a16d
Xavier Xie <xieyuehao@gmail.com>
2019-01-28 21:57:14 +1000

    151 reverse words in a string

---
+++ LeetCode/151ReverseWordsinaString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given an input string, reverse the string word by word.
+
+    //Example:  
+
+    //Input: "the sky is blue",
+    //Output: "blue is sky the".
+    //Note:
+
+    //A word is defined as a sequence of non-space characters.
+    //Input string may contain leading or trailing spaces.However, your reversed string should not contain leading or trailing spaces.
+    //You need to reduce multiple spaces between two words to a single space in the reversed string.
+    //Follow up: For C programmers, try to solve it in-place in O(1) space.
+
+
+    public class _151ReverseWordsinaString
+    {
+        public string ReverseWords(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length <= 1) return s.Trim();
+
+            var temp = s.Split(' ');
+            var sb = new StringBuilder();
+            for (int i = temp.Length - 1; i >= 0; i--)
+            {
+                if (temp[i] != "")
+                {
+                    sb.Append(temp[i].Trim());
+                    if (i > 0) sb.Append(" ");
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ LeetCodeTests/_151ReverseWordsinaStringTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _151ReverseWordsinaStringTests
+    {
+        [TestMethod()]
+        public void ReverseWordsTest()
+        {
+            //Input: "the sky is blue",
+            //Output: "blue is sky the".
+            var rw = new _151ReverseWordsinaString();
+            var result = rw.ReverseWords(" the    sky ");
+            Assert.AreEqual("sky the", result);
+        }
+    }
+}

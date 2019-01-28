05031e4ac376cadc13ea82f98ae7e5377acd4172
Xavier Xie <xieyuehao@gmail.com>
2019-01-28 23:54:24 +1000

    290 word pattern

---
+++ LeetCode/290WordPattern.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given a pattern and a string str, find if str follows the same pattern.
+
+    //Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+    //Example 1:
+
+    //Input: pattern = "abba", str = "dog cat cat dog"
+    //Output: true
+    //Example 2:
+
+    //Input:pattern = "abba", str = "dog cat cat fish"
+    //Output: false
+    //Example 3:
+
+    //Input: pattern = "aaaa", str = "dog cat cat dog"
+    //Output: false
+    //Example 4:
+
+    //Input: pattern = "abba", str = "dog dog dog dog"
+    //Output: false
+    //Notes:
+    //You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+    public class _290WordPattern
+    {
+        public bool WordPattern(string pattern, string str)
+        {
+            if (pattern == null || str == null) return false;
+            var strArr = str.Split(' ');
+            if (strArr.Length != pattern.Length) return false;
+
+            var dic = new Dictionary<char, string>();
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (dic.ContainsKey(pattern[i])){
+                    if (dic[pattern[i]].Equals(strArr[i])) continue;
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (dic.ContainsValue(strArr[i]))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        dic.Add(pattern[i], strArr[i]);
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeTests/_290WordPatternTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _290WordPatternTests
+    {
+        //Input: pattern = "abba", str = "dog cat cat dog"
+        //Output: true
+        //Example 2:
+
+        //Input:pattern = "abba", str = "dog cat cat fish"
+        //Output: false
+        //Example 3:
+
+        //Input: pattern = "aaaa", str = "dog cat cat dog"
+        //Output: false
+        //Example 4:
+
+        //Input: pattern = "abba", str = "dog dog dog dog"
+        //Output: false
+        [TestMethod()]
+        public void WordPatternTest1()
+        {
+            var wp = new _290WordPattern();
+            var pattern = "abba"; var str = "dog cat cat dog";
+            var result = wp.WordPattern(pattern, str);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod()]
+        public void WordPatternTest2()
+        {
+            var wp = new _290WordPattern();
+            var pattern = "abba"; var str = "dog cat cat fish";
+            var result = wp.WordPattern(pattern, str);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod()]
+        public void WordPatternTest3()
+        {
+            var wp = new _290WordPattern();
+            var pattern = "aaaa"; var str = "dog cat cat dog";
+            var result = wp.WordPattern(pattern, str);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void WordPatternTest4()
+        {
+            var wp = new _290WordPattern();
+            var pattern = "abba"; var str = "dog dog dog dog";
+            var result = wp.WordPattern(pattern, str);
+            Assert.AreEqual(false, result);
+        }
+    }
+}

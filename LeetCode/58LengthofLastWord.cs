e89126db4899cf107d84ad7f7b7471f3f30d24fb
Xavier Xie <xieyuehao@gmail.com>
2019-01-28 15:19:57 +1000

    58 length of last word

---
+++ LeetCode/58LengthofLastWord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+    //If the last word does not exist, return 0.
+
+    //Note: A word is defined as a character sequence consists of non-space characters only.
+
+    //Example:
+
+    //Input: "Hello World"
+    //Output: 5
+    public class _58LengthofLastWord
+    {
+        
+        public int LengthOfLastWord(string s)
+        {
+            if (s == null || s.Length == 0) return 0;
+            var res = 0;
+            s = s.Trim();
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ' || s.Length == res) break;
+                else res++;
+            }
+            return res;
+        }
+    }
+
+    //first run failed at "a " and " a", solution is to add trim
+}
---
+++ LeetCodeTests/_58LengthofLastWordTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _58LengthofLastWordTests
+    {
+        [TestMethod()]
+        public void LengthOfLastWordTest()
+        {
+            var lolw = new _58LengthofLastWord();
+            var result = lolw.LengthOfLastWord("Hello world");
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod()]
+        public void LengthOfLastWordShouldReturn5()
+        {
+            var lolw = new _58LengthofLastWord();
+            var result = lolw.LengthOfLastWord("world");
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod()]
+        public void LengthOfLastWordShouldReturn0()
+        {
+            var lolw = new _58LengthofLastWord();
+            var result = lolw.LengthOfLastWord("");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void LengthOfLastWordShouldReturn1()
+        {
+            var lolw = new _58LengthofLastWord();
+            var result = lolw.LengthOfLastWord(" a ");
+            Assert.AreEqual(1, result);
+        }
+    }
+}

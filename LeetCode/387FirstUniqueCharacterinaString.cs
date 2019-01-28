3907b6998573d4d7ba4044d724956cd908bbb2ba
Xavier Xie <xieyuehao@gmail.com>
2019-01-28 19:14:24 +1000

    287 first unique character string

---
+++ LeetCode/387FirstUniqueCharacterinaString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+    //Examples:
+
+    //s = "leetcode"
+    //return 0.
+
+    //s = "loveleetcode",
+    //return 2.
+    //Note: You may assume the string contain only lowercase letters.
+    public class _387FirstUniqueCharacterinaString
+    {
+        public int FirstUniqChar(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length == 0) return -1;
+            var result = -1;
+            var dic = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i]))  dic[s[i]] = int.MinValue;
+                else dic.Add(s[i], i);
+            }
+            foreach (var item in dic)
+            {
+                if (item.Value > int.MinValue) return item.Value;
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/_387FirstUniqueCharacterinaStringTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _387FirstUniqueCharacterinaStringTests
+    {
+        [TestMethod()]
+        public void FirstUniqCharShouldReturn2()
+        {
+            var fuc = new _387FirstUniqueCharacterinaString();
+            var reulst = fuc.FirstUniqChar("loveleetcode");
+            Assert.AreEqual(2, reulst);
+        }
+
+        [TestMethod()]
+        public void FirstUniqCharShouldReturn0()
+        {
+            var fuc = new _387FirstUniqueCharacterinaString();
+            var reulst = fuc.FirstUniqChar("leetcode");
+            Assert.AreEqual(0, reulst);
+        }
+    }
+}

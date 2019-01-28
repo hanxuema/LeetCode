c54f21a4d53e7f87171195338915e7a5fb702f30
Xavier Xie <xieyuehao@gmail.com>
2019-01-28 14:13:19 +1000

    28 implement strstr

---
+++ LeetCode/Content/28ImplementstrStr.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //    Implement strStr().
+
+    //Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+    //Example 1:
+
+    //Input: haystack = "hello", needle = "ll"
+    //Output: 2
+    //Example 2:
+
+    //Input: haystack = "aaaaa", needle = "bba"
+    //Output: -1
+    //Clarification:
+
+    //What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+    //For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+    public class _28ImplementstrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) return 0;
+
+            //hello
+            //lu
+            var result = -1;
+            for (int i = 0; i < haystack.Length - needle.Length +1; i++)
+            {
+                if (haystack.Substring(i, needle.Length).Equals(needle)) return i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/Content/_28ImplementstrStrTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _28ImplementstrStrTests
+    {
+        [TestMethod()]
+        public void StrStrTest()
+        {
+            var ss = new _28ImplementstrStr();
+            var reulst = ss.StrStr("hello", "ll");
+            Assert.AreEqual(2, reulst);
+        }
+
+        [TestMethod()]
+        public void StrStrShouldReturn0()
+        {
+            var ss = new _28ImplementstrStr();
+            var reulst = ss.StrStr("a", "a");
+            Assert.AreEqual(0, reulst);
+        }
+
+        [TestMethod()]
+        public void StrStrShouldReturnMinus1()
+        {
+            var ss = new _28ImplementstrStr();
+            var reulst = ss.StrStr("aaaaa", "bba");
+            Assert.AreEqual(-1, reulst);
+        }
+
+        [TestMethod()]
+        public void StrStrShouldReturnMinus1Again()
+        {
+            var ss = new _28ImplementstrStr();
+            var reulst = ss.StrStr("hello", "llu");
+            Assert.AreEqual(-1, reulst);
+        }
+    }
+}

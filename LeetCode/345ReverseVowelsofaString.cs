5ccfe015d5aa76487600a75313d23b4644ae1de3
Xavier Xie <xieyuehao@gmail.com>
2019-01-28 23:02:12 +1000

    345 reverse vowels

---
+++ LeetCode/345ReverseVowelsofaString.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Write a function that takes a string as input and reverse only the vowels of a string.
+
+    //Example 1:
+
+    //Input: "hello"
+    //Output: "holle"
+    //Example 2:
+
+    //Input: "leetcode"
+    //Output: "leotcede"
+    //Note:
+    //The vowels does not include the letter "y".
+
+
+    public class _345ReverseVowelsofaString
+    {
+        public string ReverseVowels(string s)
+        {
+            if (s == null || s.Length < 2) return s;
+
+            var left = 0;
+            var right = s.Length - 1;
+            var vowels = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            var sb = s.ToCharArray();
+            //2 pointer
+            while (left < right)
+            {
+                if (vowels.Contains(sb[left]) && vowels.Contains(sb[right]))
+                {
+                    var temp = sb[left];
+                    sb[left] = sb[right];
+                    sb[right] = temp;
+                    left++;
+                    right--;
+                }
+                else if (vowels.Contains(sb[left]) && !vowels.Contains(sb[right]))
+                {
+                    right--;
+                }
+                else if (!vowels.Contains(sb[left]) && vowels.Contains(sb[right]))
+                {
+                    left++;
+                }
+                else
+                {
+                    left++;
+                    right--;
+                }
+            }
+            return new String(sb);
+        }
+    }
+}
---
+++ LeetCodeTests/_345ReverseVowelsofaStringTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _345ReverseVowelsofaStringTests
+    {
+        [TestMethod()]
+        public void ReverseVowelsTest()
+        {
+            //Input: "leetcode"
+            //Output: "leotcede"
+            var rv = new _345ReverseVowelsofaString();
+            var result = rv.ReverseVowels("leetcode");
+            Assert.AreEqual("leotcede", result);
+        }
+
+        [TestMethod()]
+        public void ReverseVowelsTest2()
+        {
+            //Input: "leetcode"
+            //Output: "leotcede"
+            var rv = new _345ReverseVowelsofaString();
+            var result = rv.ReverseVowels("hello");
+            Assert.AreEqual("holle", result);
+        }
+
+        [TestMethod()]
+        public void ReverseVowelsTest3()
+        {
+            //Input: "leetcode"
+            //Output: "leotcede"
+            var rv = new _345ReverseVowelsofaString();
+            var result = rv.ReverseVowels("jkl");
+            Assert.AreEqual("jkl", result);
+        }
+
+        [TestMethod()]
+        public void ReverseVowelsTest4()
+        {
+            //Input: "leetcode"
+            //Output: "leotcede"
+            var rv = new _345ReverseVowelsofaString();
+            var result = rv.ReverseVowels(" a");
+            Assert.AreEqual(" a", result);
+        }
+
+        [TestMethod()]
+        public void ReverseVowelsTest5()
+        {
+            //Input: "leetcode"
+            //Output: "leotcede"
+            var rv = new _345ReverseVowelsofaString();
+            var result = rv.ReverseVowels("Aa");
+            Assert.AreEqual("aA", result);
+        }
+    }
+}

180962666b87df9138e6ce48cd9265b96ddaf5a9
Xavier Xie <xieyuehao@gmail.com>
2019-01-29 23:07:37 +1000

    179 largest number

---
+++ LeetCode/179LargestNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Given a list of non negative integers, arrange them such that they form the largest number.
+
+    //Example 1:
+
+    //Input: [10,2]
+    //    Output: "210"
+    //Example 2:
+
+    //Input: [3,30,34,5,9]
+    //    Output: "9534330"
+    //Note: The result may be very large, so you need to return a string instead of an integer.
+
+
+    public class _179LargestNumber
+    {
+        public string LargestNumber(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return "";
+
+            Array.Sort(nums, (a, b) => string.Compare(b + "" + a, a + "" + b));
+            return nums[0] == 0 ? "0" : string.Join("", nums);
+        }
+    }
+}
---
+++ LeetCodeTests/_179LargestNumberTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _179LargestNumberTests
+    {
+        [TestMethod()]
+        public void LargestNumberTest()
+        {
+            var ln = new _179LargestNumber();
+            var result = ln.LargestNumber(new int[] { 3, 30, 34, 5, 9 });
+            Assert.AreEqual("9534330", result);
+        }
+
+        [TestMethod()]
+        public void LargestNumberTest2()
+        {
+            var ln = new _179LargestNumber();
+            var result = ln.LargestNumber(new int[] { 10, 2 });
+            Assert.AreEqual("210", result);
+        }
+    }
+}

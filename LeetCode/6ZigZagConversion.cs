c927357a97a27a0de14371af7cdd191e41e7c151
Xavier Xie <xieyuehao@gmail.com>
2019-01-30 22:59:37 +1000

    6 zig zag conversion

---
+++ LeetCode/6ZigZagConversion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+    //P A H N
+    //A P L S I I G
+    //Y I R
+    //And then read line by line: "PAHNAPLSIIGYIR"
+
+    //Write the code that will take a string and make this conversion given a number of rows:
+
+    //string convert(string s, int numRows);
+    //    Example 1:
+
+    //Input: s = "PAYPALISHIRING", numRows = 3
+    //Output: "PAHNAPLSIIGYIR"
+    //Example 2:
+
+    //Input: s = "PAYPALISHIRING", numRows = 4
+    //Output: "PINALSIGYAHRPI"
+    //Explanation:
+
+    //P I N
+    //A L S I G
+    //Y A H R
+    //P I
+    public class _6ZigZagConversion
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows <= 1) return s;
+            StringBuilder[] sb = new StringBuilder[numRows];
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb[i] = new StringBuilder("");
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                int idx = i % (2 * numRows - 2);
+                idx = idx < numRows ? idx : 2 * numRows - 2 - idx;
+                sb[idx].Append(s[i]);
+            }
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb[0].Append(sb[i]);
+            }
+            return sb[0].ToString();
+        }
+    }
+}
---
+++ LeetCodeTests/_6ZigZagConversionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _6ZigZagConversionTests
+    {
+        [TestMethod()]
+        public void ConvertTest1()
+        {
+            var zzc = new _6ZigZagConversion();
+            var result = zzc.Convert("PAYPALISHIRING", 4);
+            Assert.AreEqual("PINALSIGYAHRPI", result);
+        }
+        [TestMethod()]
+        public void ConvertTest2()
+        {
+            var zzc = new _6ZigZagConversion();
+            var result = zzc.Convert("PAYPALISHIRING", 2);
+            Assert.AreEqual("PAHNAPLSIIGYIR", result);
+        }
+ 
+    }
+}
